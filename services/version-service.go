ccfce480b91d80127b93a78b34c2163345b6ee38
Tyrel Delaney <tyrel.hiebert@gmail.com>
2025-01-29 20:50:10 -0800

    fallback to reading version directly from file if it's not in env

--- services/version-service.go
+++ services/version-service.go
@@ -11,7 +11,7 @@ import (
 )
 
 var adjectives = []string{"red", "blue", "green", "yellow", "gold", "silver", "crystal", "sapphire", "ruby", "emerald", "firered", "leafgreen"}
-var nouns = []string{"horse", "sea", "sky", "forest", "mountain", "river", "tree", "flower", "star", "moon", "sun", "earth"}
+var nouns = []string{"horse", "sea", "sky", "forest", "mountain", "river", "tree", "flower", "star", "moon", "sun", "earth", "rocket"}
 
 var currentVersion = ""
 var versionLock sync.Mutex
@@ -38,10 +38,33 @@ func generateRandomName() string {
 }
 
 func getSemanticVersion() string {
-	data := os.Getenv("VERSION")
-	if data == "" {
-		log.Println("VERSION not set, using default version")
-		return "n.i.l"
+	envVersion := os.Getenv("VERSION")
+	if envVersion == "" {
+		fileVersion, err := getSemanticVersionFromFile()
+		if err != nil {
+			log.Println("VERSION not set, using default version")
+			return "n.i.l"
+		}
+		return fileVersion
 	}
-	return string(data)
+	log.Println("Read version from environment: " + envVersion)
+	return envVersion
+}
+
+func getSemanticVersionFromFile() (string, error) {
+	file, err := os.Open("version")
+	if err != nil {
+		log.Println("VERSION file not found, using default version")
+		return "", err
+	}
+	defer file.Close()
+	buf := make([]byte, 100)
+	n, err := file.Read(buf)
+	if err != nil {
+		log.Println("Error reading VERSION file, using default version")
+		return "", err
+	}
+	version := string(buf[:n])
+	log.Println("Read version from file: " + version)
+	return version, nil
 }
