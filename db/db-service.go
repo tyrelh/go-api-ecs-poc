975b8f00c8270a9879f87a34da9f92b385e0b72e
Tyrel Delaney <tyrel.hiebert@gmail.com>
2025-02-01 20:34:35 -0800

    setup table auto migration

--- db/db-service.go
+++ db/db-service.go
@@ -2,20 +2,23 @@ package db
 
 import (
 	"fmt"
+	"go-api-poc/models"
 	"log"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+// TODO: move database config
 var (
-	dbUser       = "go"
-	dbPassword   = "password"
-	dbHost       = "localhost"
-	dbPort       = "5000"
-	dbName       = "go_db"
-	dsn          = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName) //"go:password@tcp(127.0.0.1:5000)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	dbConnection *gorm.DB
+	dbUser              = "go"
+	dbPassword          = "password"
+	dbHost              = "localhost"
+	dbPort              = "5000"
+	dbName              = "go_db"
+	dsn                 = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName) //"go:password@tcp(127.0.0.1:5000)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dbConnection        *gorm.DB
+	shouldMigrateTables = true
 )
 
 func ConnectToDb() error {
@@ -26,5 +29,15 @@ func ConnectToDb() error {
 	}
 	dbConnection = db
 	log.Println("Connected to " + dbName + " database on " + dbHost + ":" + dbPort + " as user " + dbUser)
+
+	if shouldMigrateTables {
+		autoMigrateTables()
+	}
 	return nil
+}
+
+func autoMigrateTables() {
+	log.Println("Auto migrating tables...")
+	dbConnection.AutoMigrate(models.GetModels()...)
+	log.Println("Auto migration complete")
 }
