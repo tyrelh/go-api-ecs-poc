f3fa20946129d2afc6a2a976a99271aed0fa8636
Tyrel Delaney <tyrel.hiebert@gmail.com>
2025-02-03 19:35:14 -0800

    add some time taken logs around db queries

--- services/reward-service.go
+++ services/reward-service.go
@@ -4,6 +4,7 @@ import (
 	"go-api-poc/models"
 	"log"
 	"sync"
+	"time"
 
 	"go-api-poc/db"
 )
@@ -16,16 +17,13 @@ func GetRewards() *[]models.Reward {
 	rewardsLock.Lock()
 	defer rewardsLock.Unlock()
 
+	startTime := time.Now()
 	db := db.GetDbConnection()
 	var rewards []models.Reward
 	db.Find(&rewards)
+	log.Printf("Time taken to get rewards: %v", time.Since(startTime))
 
-	rewardList := make([]models.Reward, 0, len(rewards))
-	for _, reward := range rewards {
-		rewardList = append(rewardList, reward)
-	}
-
-	return &rewardList
+	return &rewards
 }
 
 func CreateReward(brand *string, currency *string, denomination *float32) models.Reward {
@@ -38,10 +36,12 @@ func CreateReward(brand *string, currency *string, denomination *float32) models
 		Denomination: denomination,
 	}
 
+	startTime := time.Now()
 	db := db.GetDbConnection()
 	result := db.Create(&reward)
 	log.Println("Error during creation: ", result.Error)
 	log.Println("Rows affected: ", result.RowsAffected)
+	log.Printf("Time taken to create reward: %v", time.Since(startTime))
 
 	return reward
 }
@@ -50,9 +50,11 @@ func GetReward(rewardId int) *models.Reward {
 	rewardsLock.Lock()
 	defer rewardsLock.Unlock()
 
+	startTime := time.Now()
 	db := db.GetDbConnection()
 	var reward models.Reward
 	result := db.First(&reward, rewardId)
+	log.Printf("Time taken to get reward: %v", time.Since(startTime))
 
 	if result.RowsAffected == 0 {
 		return nil
@@ -65,8 +67,10 @@ func DeleteReward(rewardId int) error {
 	rewardsLock.Lock()
 	defer rewardsLock.Unlock()
 
+	startTime := time.Now()
 	db := db.GetDbConnection()
 	db.Delete(&models.Reward{}, rewardId)
+	log.Printf("Time taken to delete reward: %v", time.Since(startTime))
 
 	return nil
 }
